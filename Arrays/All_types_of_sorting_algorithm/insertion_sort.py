f4afbebe91a8e1de18e3b0b114b35dd9912c639f
harsh saini <macos@Harshs-MacBook-Air.local>
2025-09-18 19:56:00 +0530

    Added insertion sort algorithm.

---
+++ Arrays/All_types_of_sorting_algorithm/insertion_sort.py
@@ -0,0 +1,16 @@
+"""Insertion sort is a type of sorting algorithm which also sorts array in ascending order but it shifts value one by one,
+it sorts array iteratively """
+my_array = [11,4,3,12,18,9,10,45,66]
+n = len(my_array)
+for i in range(1,n):   #loop from index 1 to n
+    insert_index = i   #currently assuming the. insert position as i 
+    current_value = my_array[i]  #storing element i in variable named as current value
+    for j in range(i-1,-1,-1):   #this loop will work from right to left 
+        if my_array[j]> current_value: 
+            my_array[j+1] = my_array[j] #j and i both will become same element as j+1 index is = index of i
+            insert_index = j  #now position to insert the element will be updated as j
+        else:
+            break
+    my_array[insert_index]= current_value #here it will insert element i at position j if j is > than i
+
+print("Sorted array: ", my_array)
